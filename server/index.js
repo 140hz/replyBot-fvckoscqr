c33f276b0bf6bcd83d1426d17eb18937cdef014a
oscar <44992155+140hz@users.noreply.github.com>
2022-06-30 13:55:10 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ var Twit = require('twit');
 var replie = [' STFU, LEAVE ME ALONE', ' a como chilla la ardilla', ' aja aja', ' el lunes sin falta carnal', ' tes chingando lol', ' vemos', ' no estoy'];
 
 var T = new Twit({
-    consumer_key:         'OWQPvEP5ugxvYNi9ZUYHY8mnl',
-    consumer_secret:      'pUWKwGqmBBof8sIQ0zIDWgksA0l5MQYlgBGIcn7RdO52iPWVHo',
-    access_token:         '2422708117-iGpySGUt8mV2ANcTvxmV49ec0ywgLDs5ab10879',
-    access_token_secret:  'usa6kDYZUyhil35R4ZBtyT4BMkZqFD1T9LqxpYT1TUvm2',
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
        // optional - requires SSL certificates to be valid.
 })
 
